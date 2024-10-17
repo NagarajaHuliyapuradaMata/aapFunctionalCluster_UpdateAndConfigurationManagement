d48ade14ce0004d20e72aca2720c7e77bc3ad1b8
Nagaraja HULIYAPURADA MATA <nagaraja.huliyapurada-mata@outlook.com>
2024-10-17 22:15:49 +0530

    Initial interface - Update and Configuration Management

--- main_LATEST/aapFunctionalCluster_UpdateAndConfigurationManagement.cpp
+++ main_LATEST/aapFunctionalCluster_UpdateAndConfigurationManagement.cpp
@@ -7,6 +7,7 @@
 /******************************************************************************/
 /* #INCLUDES                                                                  */
 /******************************************************************************/
+#include "interface_UpdateAndConfigurationManagement_PackageManagement.hpp"
 
 /******************************************************************************/
 /* #DEFINES                                                                   */
@@ -19,6 +20,28 @@
 /******************************************************************************/
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
+class aapFunctionalCluster_UpdateAndConfigurationManagement:
+      public  interface_UpdateAndConfigurationManagement_PackageManagement
+{
+   public:
+      void Activate                  (void);
+      void Cancel                    (void);
+      void DeleteTransfer            (void);
+      void Finish                    (void);
+      void GetHistory                (void);
+      void GetId                     (void);
+      void GetSwClusterChangeInfo    (void);
+      void GetSwClusterInfo          (void);
+      void GetSwClusterManifestInfo  (void);
+      void GetSwPackages             (void);
+      void GetSwProcessProgress      (void);
+      void ProcessSwPackage          (void);
+      void RevertProcessedSwPackages (void);
+      void Rollback                  (void);
+      void TransferData              (void);
+      void TransferExit              (void);
+      void TransferStart             (void);
+};
 
 /******************************************************************************/
 /* CONSTS                                                                     */
@@ -35,6 +58,56 @@
 /******************************************************************************/
 /* FUNCTIONS                                                                  */
 /******************************************************************************/
+void aapFunctionalCluster_UpdateAndConfigurationManagement::Activate(void){
+}
+
+void aapFunctionalCluster_UpdateAndConfigurationManagement::Cancel(void){
+}
+
+void aapFunctionalCluster_UpdateAndConfigurationManagement::DeleteTransfer(void){
+}
+
+void aapFunctionalCluster_UpdateAndConfigurationManagement::Finish(void){
+}
+
+void aapFunctionalCluster_UpdateAndConfigurationManagement::GetHistory(void){
+}
+
+void aapFunctionalCluster_UpdateAndConfigurationManagement::GetId(void){
+}
+
+void aapFunctionalCluster_UpdateAndConfigurationManagement::GetSwClusterChangeInfo(void){
+}
+
+void aapFunctionalCluster_UpdateAndConfigurationManagement::GetSwClusterInfo(void){
+}
+
+void aapFunctionalCluster_UpdateAndConfigurationManagement::GetSwClusterManifestInfo(void){
+}
+
+void aapFunctionalCluster_UpdateAndConfigurationManagement::GetSwPackages(void){
+}
+
+void aapFunctionalCluster_UpdateAndConfigurationManagement::GetSwProcessProgress(void){
+}
+
+void aapFunctionalCluster_UpdateAndConfigurationManagement::ProcessSwPackage(void){
+}
+
+void aapFunctionalCluster_UpdateAndConfigurationManagement::RevertProcessedSwPackages(void){
+}
+
+void aapFunctionalCluster_UpdateAndConfigurationManagement::Rollback(void){
+}
+
+void aapFunctionalCluster_UpdateAndConfigurationManagement::TransferData(void){
+}
+
+void aapFunctionalCluster_UpdateAndConfigurationManagement::TransferExit(void){
+}
+
+void aapFunctionalCluster_UpdateAndConfigurationManagement::TransferStart(void){
+}
 
 /******************************************************************************/
 /* EOF                                                                        */
--- main_LATEST/interface_UpdateAndConfigurationManagement_PackageManagement.hpp
+++ main_LATEST/interface_UpdateAndConfigurationManagement_PackageManagement.hpp
@@ -20,6 +20,30 @@
 /******************************************************************************/
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
+typedef unsigned char uint8;
+
+class interface_UpdateAndConfigurationManagement_PackageManagement{
+   public:
+      uint8 CurrentStatus;
+
+      virtual void Activate                  (void) = 0;
+      virtual void Cancel                    (void) = 0;
+      virtual void DeleteTransfer            (void) = 0;
+      virtual void Finish                    (void) = 0;
+      virtual void GetHistory                (void) = 0;
+      virtual void GetId                     (void) = 0;
+      virtual void GetSwClusterChangeInfo    (void) = 0;
+      virtual void GetSwClusterInfo          (void) = 0;
+      virtual void GetSwClusterManifestInfo  (void) = 0;
+      virtual void GetSwPackages             (void) = 0;
+      virtual void GetSwProcessProgress      (void) = 0;
+      virtual void ProcessSwPackage          (void) = 0;
+      virtual void RevertProcessedSwPackages (void) = 0;
+      virtual void Rollback                  (void) = 0;
+      virtual void TransferData              (void) = 0;
+      virtual void TransferExit              (void) = 0;
+      virtual void TransferStart             (void) = 0;
+};
 
 /******************************************************************************/
 /* CONSTS                                                                     */
